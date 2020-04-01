fe254236959bf7ac2e754ba43cb410b6f3cc8fa6
chloeych <52624099+chloeych@users.noreply.github.com>
2020-04-01 09:51:20 -0400

    port update

--- app.js
+++ app.js
@@ -3,10 +3,19 @@ const express = require('express');
 // Initiate Express to App 
 const app = express(); 
 // Set the Port
-const port = 4000;
+const port = process.env.PORT || 4000;
 // Create base route
 app.get('/', (req, res)=> res.send('Exercise Four'));
 // Set up app so that it runs when this file is run 
 app.listen(port, () => 
     console.log(`Example app listening at http://localhost:${port}`)
 );
+
+
+// Cloud Server Hosing 
+
+// * Important that information is shared on the same port 
+// * Dyno - Instance of a cloud server
+// * Specify a port variable that they will set automatically for you
+// * $PORT is a dynamic value that is set automatically 
+// * Make sure you're not hardcoding the 'port' 
