90015066b366b207587593d5d76b3d0bfbfb5024
chloeych <52624099+chloeych@users.noreply.github.com>
2020-04-06 09:29:32 -0400

    second commit

--- app.js
+++ app.js
@@ -1,11 +1,33 @@
 // Import Express 
 const express = require('express');
+
 // Initiate Express to App 
 const app = express(); 
 // Set the Port - if environmental.PORT use that, otherwise use 4000;
 const port = process.env.PORT || 4000;
+
+
+
+//Require Firebase
+const firebase = require('firebase/app');
+// get configuration object so we can communicate with Firebase
+const firebaseConfig = {
+    apiKey: "AIzaSyB_PcWgQ9V9f1vflY-2wa_4F3VYtqS_deI",
+    authDomain: "exercise-4-1708d.firebaseapp.com",
+    databaseURL: "https://exercise-4-1708d.firebaseio.com",
+    projectId: "exercise-4-1708d",
+    storageBucket: "exercise-4-1708d.appspot.com",
+    messagingSenderId: "990670199477",
+    appId: "1:990670199477:web:68c4566b23d009f7d4bcfc"
+  };
+
 // Create base route
-app.get('/', (req, res)=> res.send('Exercise Four'));
+app.get('/', (req, res)=> 
+    res.send([
+        "Running in Browser"
+    ])
+    );
+    
 // Set up app so that it runs when this file is run 
 app.listen(port, () => 
     console.log(`Example app listening at http://localhost:${port}`)
@@ -19,3 +41,6 @@ app.listen(port, () =>
 // * Specify a port variable that they will set automatically for you
 // * $PORT is a dynamic value that is set automatically 
 // * Make sure you're not hardcoding the 'port' 
+
+
+// https://blueberry-sundae-97148.herokuapp.com/
